6ee904bb85782db8434643e219f4a87209b72619
Mattia Risso <rissomattia3@gmail.com>
2026-08-04 21:39:59 -0300

    feat: integra docker-compose con Prometheus y Grafana para observabilidad LLMOps

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from app.api.routes import router
-from app.core.middlewares import TimingMiddleware  # <-- Nueva importación
+from app.core.middlewares import TimingMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator # <-- 1. Importación
 
 app = FastAPI(title="LLMOps Proxy", version="1.0.0")
 
-# Activamos el middleware (se ejecuta en TODAS las peticiones)
-app.add_middleware(TimingMiddleware)  # <-- Activación
-
-# Conectamos las rutas
+app.add_middleware(TimingMiddleware)
 app.include_router(router, prefix="/api/v1")
+
+# <-- 2. Activamos el instrumentador de métricas
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
